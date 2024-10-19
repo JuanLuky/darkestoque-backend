f5aa7514c3b0ba17882711c646a94f1549492a86
Juan Carlos <juandev33@gmail.com>
2024-10-19 15:50:32 -0300

    teste

--- src/main/java/com/jc/darkestoque/cors/WebConfig.java
+++ src/main/java/com/jc/darkestoque/cors/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:4200")  // Defina explicitamente as origens permitidas
+                .allowedOriginPatterns("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);  // Credenciais só podem ser permitidas para origens específicas
